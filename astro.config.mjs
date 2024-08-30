4f201ba013ab75f18977abe6dca21c3bd1e38d4d
mieras <mieras@gmail.com>
2024-08-30 13:27:40 +0200

    cdn true server hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,45 +8,40 @@ const {
   PUBLIC_SANITY_DATASET,
 } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 import { defineConfig } from "astro/config";
-import isPreviewMode from './src/utils/isPreviewMode';
+// import isPreviewMode from './src/utils/isPreviewMode';
 
 // Different environments use different variables
 const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
 const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 
-
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
 // Change this depending on your hosting provider (Vercel, Netlify etc)
-// https://docs.astro.build/en/guides/server-side-rendering/#adding-an-adapter
-// import vercel from "@astrojs/vercel/serverless";
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   // Hybrid+adapter is required to support embedded Sanity Studio
   // output: "hybrid",
-  output: 'server',
+  output: 'hybrid',
   adapter: netlify({
-    imageCDN: false,
+    imageCDN: true,
   }),
   integrations: [sanity({
     projectId,
     dataset,
     studioBasePath: "/admin",
-      
     useCdn: false,
     // `false` if you want to ensure fresh data
     apiVersion: "2023-05-31", // Set to date of setup to use the latest API version
-    perspective: isPreviewMode? 'previewDrafts' : 'published', 
-    token: isPreviewMode ? import.meta.env.PUBLIC_SANITY_AUTH_TOKEN : undefined,
+    // perspective: isPreviewMode? 'previewDrafts' : 'published', 
+    // token: isPreviewMode ? import.meta.env.PUBLIC_SANITY_AUTH_TOKEN : undefined,
     // ignoreBrowserTokenWarning: isPreviewMode ? true : false
 
   }), react() // Required for Sanity Studio
   ]
 });
-
 
 
 import { createClient } from "@sanity/client";
