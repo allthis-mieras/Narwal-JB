acea46bc1c123c630fdc508342b76638acef0f33
mieras <mieras@gmail.com>
2024-09-05 21:45:08 +0200

    swiper autoplay

--- src/utils/slider_simple.js
+++ src/utils/slider_simple.js
@@ -1,16 +1,28 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { EffectFade, Autoplay, Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
 
 
 document.addEventListener( 'DOMContentLoaded', function() {
     const swiper = new Swiper('.swiper', {
-        modules: [Navigation, Pagination],
+        modules: [Navigation, Pagination, EffectFade, Autoplay],
+        
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
-        }
+        },
+        loop: true,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        autoplay: {
+   delay: 5000,
+ },
+ speed: 500,
       });
 });
